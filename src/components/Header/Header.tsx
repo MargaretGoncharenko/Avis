9ca73470315c9ffce5e7e0e101005be7f2e5b0d8
MargaretGoncharenko <margaret.goncharenko@gmail.com>
2021-11-29 18:55:40 +0300

    layout of profile page, posts with props

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import h from "./Header.module.css";
+
+export const Header = () => {
+    return(
+            <header className={h.header}>
+                <img src="https://us.123rf.com/450wm/marsono/marsono1806/marsono180600239/102493063-bird-logo-template-vector-illustration.jpg?ver=6"/>
+            </header>
+    )
+}
---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import mp from "./MyPosts.module.css";
+import {Post} from "./Post/Post";
+
+export const MyPosts = () => {
+    return(
+            <div>
+                My posts
+                <div>
+                    <textarea className={mp.textarea}
+                              placeholder="Post something!"/>
+                    <button>Add post</button>
+                </div>
+                <div className="posts">
+                  <Post message="Hi! How are you?" likesCount={21}/>
+                  <Post message="It's my first post." likesCount={28}/>
+
+                </div>
+            </div>
+    )
+}
