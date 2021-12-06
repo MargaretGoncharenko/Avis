0b45f67d66348f99b95eee52d171de293f904e5d
MargaretGoncharenko <margaret.goncharenko@gmail.com>
2021-12-06 14:51:52 +0300

    Refactoring profile page and dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,33 +2,45 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import d from "./Dialogs.module.css";
 
+type DialogItemProps = {
+    name: string
+    id: string
+}
+const DialogItem = (props: DialogItemProps) => {
+    let path = "/messages/" + props.id;
+    return (
+        <div className={d.dialogs + ' ' + d.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+type MessageProps = {
+    message: string
+}
+const Message = (props: MessageProps) => {
+    return (
+        <div className={d.message}>{props.message}</div>
+    )
+}
+
 export const Dialogs = () => {
     return (
         <div className={d.dialogs}>
             <div className={d.dialogsItems}>
-                <div className={d.dialogs + ' ' + d.active}>
-                    <NavLink to="/messages/1">Andrey</NavLink>
-                </div>
-                <div className={d.dialogs + ' ' + d.active}>
-                    <NavLink to="/messages/2">Valery</NavLink>
-                </div>
-                <div className={d.dialogs + ' ' + d.active}>
-                    <NavLink to="/messages/3">Anna</NavLink>
-                </div>
-                <div className={d.dialogs + ' ' + d.active}>
-                    <NavLink to="/messages/4">Ira</NavLink>
-                </div>
-                <div className={d.dialogs + ' ' + d.active}>
-                    <NavLink to="/messages/5">Katya</NavLink>
-                </div>
-                <div className={d.dialogs + ' ' + d.active}>
-                    <NavLink to="/messages/6">Maxim</NavLink>
-                </div>
+                <DialogItem name="Andrey" id="1"/>
+                <DialogItem name="Valery" id="2"/>
+                <DialogItem name="Anna" id="3"/>
+                <DialogItem name="Ira" id="4"/>
+                <DialogItem name="Katya" id="5"/>
+                <DialogItem name="Maxim" id="6"/>
+
             </div>
             <div className={d.messages}>
-                <div className={d.message}>hello</div>
-                <div className={d.message}>hey</div>
-                <div className={d.message}>how are you?</div>
+                <Message message="hello"/>
+                <Message message="hey"/>
+                <Message message="how are you?"/>
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,16 +5,18 @@ import {Post} from "./Post/Post";
 export const MyPosts = () => {
     return (
         <div>
-            My posts
             <div>
+                <div>
                     <textarea className={mp.textarea}
                               placeholder="Post something!"/>
-                <button>Add post</button>
+                </div>
+                <div>
+                    <button className={mp.addBtn}>Add post</button>
+                </div>
             </div>
-            <div className="posts">
-                <Post message="Hi! How are you?" likesCount={21}/>
-                <Post message="It's my first post." likesCount={28}/>
-
+            <div className={mp.posts}>
+                <div><Post message="Hi! How are you?" likesCount={21}/></div>
+                <div><Post message="It's my first post." likesCount={28}/></div>
             </div>
         </div>
     )
